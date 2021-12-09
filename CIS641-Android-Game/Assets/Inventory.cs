84a2d5dda71262e7d56ecc293c5b9a660ead5b74
Lauren-Vanderklok <vandela1@mail.gvsu.edu>
2021-12-08 19:47:24 -0500

    merged last fixes. added final and starting scenes

--- CIS641-Android-Game/Assets/Inventory.cs
+++ CIS641-Android-Game/Assets/Inventory.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
+
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class Inventory : MonoBehaviour
 {
@@ -13,19 +17,32 @@ public class Inventory : MonoBehaviour
     public GameObject panel;
     public GameObject content;
     public GameObject imagePrefab;
+    
 
-    private List<Key> inventory;
+    private List<string> inventory;
+    private List<string> alreadyDisplayed;
+    private string savedInventoryPath = "test.txt";
 
     public void addKey(Key key)
     {
-        inventory.Add(key);
+        inventory.Add(key.ID);
+
+        using (StreamWriter writer = new StreamWriter(savedInventoryPath))
+        {
+            foreach (string k in inventory)
+            {
+                writer.WriteLine(k);
+            }
+            
+        }
+
     }
 
     public bool hasKey(string ID)
     {
-        foreach (Key k in inventory)
+        foreach (string k in inventory)
         {
-            if (k.ID == ID)
+            if (k == ID)
             {
                 return true;
             }
@@ -46,14 +63,27 @@ public class Inventory : MonoBehaviour
         {
             panel.SetActive(true);
 
-            foreach (Key key in inventory)
+            foreach (string key in inventory)
             {
-                Transform contentTransform = content.GetComponent<Transform>();
+                bool displayed = false;
+                foreach (string prevKey in alreadyDisplayed)
+                {
+                    if (key == prevKey)
+                    {
+                        displayed = true;
+                        break;
+                    }
+                }
+                if (!displayed)
+                {
+                    Transform contentTransform = content.GetComponent<Transform>();
 
-                GameObject newObj = (GameObject)Instantiate(imagePrefab, contentTransform);
+                    GameObject newObj = (GameObject)Instantiate(imagePrefab, contentTransform);
 
-                Image NewImage = newObj.GetComponent<Image>();
-                NewImage.sprite = Resources.Load<Sprite>("Keys/" + key.ID);
+                    Image NewImage = newObj.GetComponent<Image>();
+                    NewImage.sprite = Resources.Load<Sprite>("Keys/" + key);
+                    alreadyDisplayed.Add(key);
+                }
 
             }
         }
@@ -63,7 +93,9 @@ public class Inventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        inventory = new List<Key>();
+
+        inventory = new List<string>();
+        alreadyDisplayed = new List<string>();
 
         if (panel == null)
         {
@@ -76,6 +108,45 @@ public class Inventory : MonoBehaviour
         }
 
         panel.SetActive(false);
+
+        // FileStream F = new FileStream("test.txt", FileMode.OpenOrCreate, FileAccess.Read);
+
+        if (!File.Exists(savedInventoryPath))
+        {
+            StreamWriter writer = File.CreateText(savedInventoryPath);
+        }
+        else
+        {
+            using (StreamReader reader = File.OpenText(savedInventoryPath))
+            {
+                string s = reader.ReadToEnd();
+
+                string[] keyIDs = s.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                //int count = 0;
+                foreach (string id in keyIDs)
+                {
+                    inventory.Add(id);
+
+                    //Key key = new Key(id);
+                    //addKey(key);
+                   // count++;
+                }
+
+                string o = reader.ReadToEnd();
+
+                //foreach (var token in s.Tokenize('\n'))
+                //{
+                //    // Access the tokens here...
+                //}
+
+                //while ((s = reader.ReadLine()) != null)
+                //{
+                //    Console.WriteLine(s);
+
+                //}
+            }
+        }
+
     }
 
     // Update is called once per frame
